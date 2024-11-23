3b9b3ec925962e588b6dff2faacfba5df03cfb11
Richard B Mezzomo <richardmezzomo1@gmail.com>
2024-11-23 19:33:19 -0300

    ..

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import * as cartService from "./services/cart.js";
+import createItem from "./services/item.js";
+
+const myCart = [];
+const myWhishList = [];
+
+console.log("Welcome to the your Shopee Cart!");
+
+const item1 = await createItem("hotwheels ferrari", 20.99, 1);
+const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
+
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+
+await cartService.displaycart(myCart);
+// await cartService.deleteItem(myCart, item2.name);
+// await cartService.deleteItem(myCart, item1.name);
+await cartService.calculateTotal(myCart);
